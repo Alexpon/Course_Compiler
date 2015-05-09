d14553d40a6bdae324edec236fcf5191bcd4ce75
Alexpon <ponpon88810@gmail.com>
2015-05-09 11:27:17 +0800

    Final src

---
+++ src/main.c
@@ -0,0 +1,34 @@
+int x ;
+int main ( )
+{
+    int a ;
+	double b ;
+	char c ;	
+	float d ;
+	int e [ 20 ] ;
+	a = 1 ;
+	b = 2 ;
+	c = 3 ;
+	d = 4 ;
+    a = b * c ;
+    b = c / d ;
+    a == c ;
+    a != d ;
+    a < d ;
+	if ( a <= b )
+		print b ;
+	else {
+		char x ;
+		! a ;	
+	}
+	while ( - a  )
+		a = 3 ;
+	return 0 ; 
+}
+
+int hello ( int x ) {
+	double y ;
+	int c ;
+	c = 0 ;
+	return c ;
+}
