26923117da359b13bb4806153fd6ef45f281dc2a
Alexpon <ponpon88810@gmail.com>
2015-05-09 11:03:02 +0800

    Final version
    pre-version has a little logical errors
    add more test files
    change the folder

---
+++ src/main2.c
@@ -0,0 +1,22 @@
+int main ( )
+{
+	int a ;
+	char b ;
+	double c ;
+	float d ;
+	a = a + 1 ;
+    	a + 2 ;
+    	b - 3 ;
+    	3 * 4 ;
+    	5 / 3 ;
+    	y == b ;
+    	o != c ;
+    	n < c ;
+    	a <= b ;
+    	a > c ;
+    	a >= b ;
+    	a && b ;
+    	a || c ;
+	- a ;
+	! b ;
+}
